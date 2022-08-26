704b6bdefe59681775d4132f8718dcea433bb666
YuriiSenenko <yurkosenenko@gmail.com>
2022-08-26 16:35:28 +0300

    Gallery 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.addEventListener('click', onClickImage);
+
+//змінна - результат виклику функції
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+//Підключаю розмітку в HTML
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+//Створюю розмітку галереї
+function createGalleryItemsMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `<a class="gallery__item" href="${original}">
+	<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+		})
+		.join('');
+}
+
+function onClickImage(event) {
+	event.preventDefault();
+	const imgItem = event.target.classList.value.includes('gallery__image');
+	if (!imgItem) return;
+
+	const currentImgLink = event.target.dataset.source;
+	new SimpleLightbox('.gallery a', {
+		animationSpeed: 150,
+		captionsData: 'alt',
+		captionDelay: 50,
+	});
+}
